797e98710bc800d37a949fadfb0b795f8d8b0ee1
Scott Krulcik <skrulcik@andrew.cmu.edu>
2016-03-24 11:24:51 -0400

    Updated colors
    - Random color background
    - White navbar instead of black

--- js/main.js
+++ js/main.js
@@ -2,3 +2,30 @@ $('.button-collapse').sideNav({
       menuWidth: 400,
       closeOnClick: true
     });
+
+
+var backgroundColors = [
+    "red",
+    "pink",
+    "purple",
+    "deep-purple",
+    "indigo",
+    "blue",
+    "light-blue",
+    "cyan",
+    "teal",
+    "green",
+    "light-green",
+    "lime",
+    "yellow",
+    "amber",
+    "orange",
+    "deep-orange",
+    "brown",
+    "grey",
+    "blue-grey",
+    "black",
+    "white"
+];
+
+$('body').addClass(backgroundColors[Math.floor(Math.random()*backgroundColors.length)]);
