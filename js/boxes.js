36eda505a6b82f923179c3c2d16e9a0c314035f4
Scott Krulcik <skrulcik@gmail.com>
2018-03-13 13:52:42 -0400

    Boxes animation V1 (#2)
    * Update Ruby packages
    
    * Boxes try 1: webGL
    
    * Initial p5js version
    
    * More polished p5js version, annoyed by lack of gradients
    
    * Parametrize colors and prepare for shadows
    
    * Colors can be more easily adjusted at the top of the file
    * Drawing order of boxes is more favorable for shadows
    
    * Refine box sizes and drawing
    
    * Change to a blue top, green sides color scheme
    
    * Clean up 3D effect and shadows
    
    * Clarify tile drawing function with a comment

---
+++ js/boxes.js
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Colors & Fills - set in setup() b/c P5
+ ******************************************************************************/
+var TOP_COLOR;
+var FRONT_COLOR;
+var SIDE_COLOR;
+
+/*******************************************************************************
+ * Constants
+ ******************************************************************************/
+var horizontalBoxCount = 15;
+var verticalBoxCount = 15;
+var boxWidth;
+var boxHeight;
+var maxPop;
+
+function computeBoxSize() {
+    // - 1 makes sure the boxes go just beyond the screen
+    boxWidth = ceil(windowWidth / (horizontalBoxCount - 1));
+    boxHeight = ceil(windowHeight / (verticalBoxCount - 1));
+    maxPop = boxHeight * 1.5;
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    computeBoxSize();
+
+    TOP_COLOR = color(85, 25, 255);
+    FRONT_COLOR = color(0, 255, 20);
+    SIDE_COLOR = color(20, 180, 30);
+    drawingContext.shadowColor = "#222"
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    computeBoxSize();
+}
+
+/* Draws a the 3D box for the given "tile" on the screen. The term "tile" is
+ * used because even though the tile is described by a rectangle on the screen,
+ * the 3D box may extend beyond the bounds of the rectangle. The "tiled" look
+ * will always be maintained, though, assuming left-right to top-down drawing
+ * order. */
+function drawTile(x, y, w, h, boxOffset) {
+    y -= boxOffset;
+    var m = 20; // margin
+    noStroke();
+    // Add shadow for the top if the box is popped up, but let it look
+    // homogeneous when the boxes aren't popped
+    if (boxOffset > 10) {
+        var offset = map(boxOffset, 1, maxPop, 0, -5, true);
+        drawingContext.shadowOffsetY = offset;
+        drawingContext.shadowBlur = 10;
+    }
+    // Top
+    fill(TOP_COLOR);
+    quad(x, y,
+        x + w, y,
+        x + w - m, y + h,
+        x - m, y + h);
+    // Turn off shadow
+    drawingContext.shadowOffsetY = 0;
+    drawingContext.shadowBlur = 0;
+    // Front
+    fill(FRONT_COLOR);
+    quad(x - m, y + 3 * h,
+        x + w - m, y + 3 * h,
+        x + w - m, y + h,
+        x - m, y + h);
+    // Side
+    fill(SIDE_COLOR);
+    quad(x + w, y,
+        x + w - m, y + h,
+        x + w - m, y + 3 * h,
+        x + w, y + 2 * h);
+}
+
+/* Gives the distance between two points scaled to "boxs" instead of standard
+ * pixels. */
+function boxDist(x1, y1, x2, y2) {
+    return sqrt(((x1 - x2) / boxWidth) ** 2 +  ((y1 - y2) / boxHeight) ** 2);
+}
+
+function draw() {
+    clear();
+    for (var r = 0; r < verticalBoxCount; r++) {
+        for (var c = 0; c < horizontalBoxCount; c++) {
+            // Top left
+            x = c * boxWidth;
+            y = r * boxHeight;
+
+            // Center
+            cx = x + boxWidth / 2;
+            cy = y + boxHeight / 2;
+
+            // Distance between center and mouse from mouse, never 0!
+            d = boxDist(cx, cy, mouseX, mouseY) + 0.0000000001;
+
+            // Pop up boxes that are within ~4 of the mouse
+            boxOffset = min(maxPop, boxHeight / d**1.5);
+
+            drawTile(x, y, boxWidth, boxHeight, boxOffset);
+        }
+    }
+}
+
